782058a17658486a6d07373c0d1cf7df7f0aa50e
xtt28 <155261788+xtt28@users.noreply.github.com>
2026-01-10 07:06:44 -0500

    Replace PNG logo with SVG and text.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-    import logo from "$lib/assets/open-reporting-platform.png";
+    import logo from "$lib/assets/open-reporting-platform.svg";
 </script>
 
 <header
-	class="w-full text-center flex flex-col justify-center py-3 mt-10 mb-5"
+	class="w-full flex flex-col justify-center py-3 mt-10 mb-5"
 >
 	<a href="/">
-		<img src={logo} alt="myBCA Open Reporting Platform logo" class="w-96" />
+        <div class="flex flex-col gap-2">
+            <img src={logo} alt="myBCA Open Reporting Platform logo" class="w-20">
+            <p class="text-2xl">
+                <span class="font-bold">myBCA</span>
+                Open Reporting Platform
+            </p>
+        </div>
 	</a>
 </header>
